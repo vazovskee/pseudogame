0d0712eaa7572abcc1e6663fc5b740ecd0eee72b
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-30 23:17:29 +0300

    contract Archer was added

---
+++ Archer.sol
@@ -0,0 +1,13 @@
+
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+import "BattleUnit.sol";
+
+contract Archer is BattleUnit {
+
+    int16 public constant ARCHER_DEFAULT_ARMOR_POINTS = 2;
+    int16 private constant ARCHER_DEFAULT_ATACK_POWER = 15;
+
+    constructor(Fortress newFortress) BattleUnit(newFortress, ARCHER_DEFAULT_ARMOR_POINTS, ARCHER_DEFAULT_ATACK_POWER) public {}
+}
